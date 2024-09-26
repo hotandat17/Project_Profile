6f728d295171ff8b6b15b0e18409a73a0ec57d5f
hotandat17 <hotandat17@gmail.com>
2024-09-26 11:37:33 +0700

    sua lai giao dien

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,17 +8,19 @@ import { store, persistor } from '@/redux/store';
 import StoreProvider from "./storeProvider";
 import RootLayoutClient from './Persist';
 import { Metadata } from 'next'
+import { connection } from '../../config/data';
 export const metadata = {
   title: 'HDStore',
   description: 'HD là nơi giới thiệu về profile cá nhân và cũng là nơi chỉa sẽ khóa học và ứng dụng free !!! (100% free)'
 }
 
 const inter = Inter({ subsets: ["latin"] });
-
+await connection()
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+
         <RootLayoutClient>
           {children}
         </RootLayoutClient>
